f7c39d58c9c54d12bbdd04de617b2664aa1b6b0f
devmegan <megan.molloy@canalrivertrust.org.uk>
2022-04-18 08:11:28 +0000

    add links to config

---
+++ src/Config.js
@@ -0,0 +1,28 @@
+const LINKS = {
+    useful: [
+        {
+            name: 'Blog',
+            href: '/blog',
+            type: '',
+        },
+        {
+            name: 'Contact',
+            href: '/contact',
+            type: 'author',
+        }
+    ],
+    connect: [
+        {
+            name: 'Twitter',
+            href: 'https://twitter.com/devmegan_/',
+            type: 'external',
+        },
+        {
+            name: 'LinkedIn',
+            href: 'https://www.linkedin.com/in/devmegan/',
+            type: 'external',
+        },
+    ]
+}
+
+export { LINKS };
