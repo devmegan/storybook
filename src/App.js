95f60239d6fc85d754e6e59b80994b161f42b2a2
devmegan <megan.molloy@canalrivertrust.org.uk>
2022-04-18 12:05:43 +0000

    add storybook instruction to main app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import Footer from './components/Footer';
-
 export default function App() {
   return (
-      <Footer />
-    );
+    <div className="text-center mt-20">
+      <h1 className="text-2xl">use <span className="font-mono bg-gray-200 text-black rounded-md p-2">npm run storybook</span> to see my components</h1>
+    </div>
+  );
 }
