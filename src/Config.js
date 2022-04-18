b4bd86d8fdd1eb6f1ab07310fb23d5e68be24e71
devmegan <megan.molloy@canalrivertrust.org.uk>
2022-04-18 12:08:03 +0000

    add hero content to config

--- src/Config.js
+++ src/Config.js
@@ -25,4 +25,13 @@ const LINKS = {
     ]
 }
 
-export { LINKS };
+const HERO_CONTENT = {
+    header: <>Hi, I'm <strong>Megan</strong>. A Web Developer with:</>,
+    list: [
+        'Hands-on, full-stack experience across public, private and third sector projects.',
+        <>A <a className="link link-hover font-semibold underline text-secondary hover:text-secondary-focus" href="/">GitHub</a> account.</>,    
+        <>A downloadable <a className="link link-hover font-semibold underline text-secondary hover:text-secondary-focus" href="/">CV</a>.</>,        
+    ],
+    CTA: <>Want to see what tech I enjoy working with? Check out my <a className="link link-hover font-semibold underline text-secondary hover:text-secondary-focus" href="https://github.com/devmegan/awesome" target="_blank" rel="noreferrer">awesome list</a>. 😎</>
+}
+export { LINKS, HERO_CONTENT };
