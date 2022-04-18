6e123c0c4d2606045ef3e3cee7571f73257927da
devmegan <megan.molloy@canalrivertrust.org.uk>
2022-04-18 16:50:02 +0000

    update storybook preview layout

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,13 +2,8 @@ import "../src/index.css"
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
   backgrounds: {
     default: 'light',
   },
+  layout: 'centered',
 }
