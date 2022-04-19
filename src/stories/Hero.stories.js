13b669e160dfa35c23b344b8121068d89bb23c1f
devmegan <megan.molloy@canalrivertrust.org.uk>
2022-04-19 09:23:24 +0000

    update hero layout

--- src/stories/Hero.stories.js
+++ src/stories/Hero.stories.js
@@ -16,12 +16,8 @@ const Light = Template.bind();
 export { Light, Dark };
 
 const content = {
-    header: <>Hi, I'm <strong>Megan</strong>. A Web Developer with:</>,
-    list: [
-        'Hands-on, full-stack experience across public, private and third sector projects.',
-        <>A <a className="link link-hover font-semibold underline text-secondary hover:text-secondary-focus" href="/">GitHub</a> account.</>,    
-        <>A downloadable <a className="link link-hover font-semibold underline text-secondary hover:text-secondary-focus" href="/">CV</a>.</>,        
-    ],
+    header: 'Hi, I\'m Megan',
+    para: 'A Web Developer with hands-on, full-stack experience across public, private and third sector projects.',
     CTA: <>Want to see what tech I enjoy working with? Check out my <a className="link link-hover font-semibold underline text-secondary hover:text-secondary-focus" href="https://github.com/devmegan/awesome" target="_blank" rel="noreferrer">awesome list</a>. 😎</>
 }
 
