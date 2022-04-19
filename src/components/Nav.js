f6e7696234c8b6e1d3b501ec20461fee31d30a74
devmegan <megan.molloy@canalrivertrust.org.uk>
2022-04-19 09:20:15 +0000

    add nav

---
+++ src/components/Nav.js
@@ -0,0 +1,16 @@
+export default function Nav(){
+    return (
+        <div class="navbar px-0 bg-base-100 rounded-md mb-20 md:mb-40">
+            <div class="flex-1">
+                <a class="btn btn-secondary normal-case text-2xl" href="/">M</a>
+            </div>
+            <div class="flex-none hidden md:block">
+                <ul class="menu menu-horizontal p-0 font-medium">
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#blog">Blog</a></li>
+                <li><a href="#contact">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    )
+}
