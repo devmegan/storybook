03f30fda47b01a8f4ffaa4a96b7d515afca57d12
devmegan <megan.molloy@canalrivertrust.org.uk>
2022-04-19 09:22:43 +0000

    add data to config

--- src/Config.js
+++ src/Config.js
@@ -28,12 +28,8 @@ const LINKS = {
 const GITHUB_LINK = 'https://github.com/devmegan';
 
 const HERO_CONTENT = {
-    header: <>Hi, I'm <strong>Megan</strong>. A Web Developer with:</>,
-    list: [
-        'Hands-on, full-stack experience across public, private and third sector projects.',
-        <>A <a className="link link-hover font-semibold underline text-secondary hover:text-secondary-focus" href="/">GitHub</a> account.</>,    
-        <>A downloadable <a className="link link-hover font-semibold underline text-secondary hover:text-secondary-focus" href="/">CV</a>.</>,        
-    ],
-    CTA: <>Want to see what tech I enjoy working with? Check out my <a className="link link-hover font-semibold underline text-secondary hover:text-secondary-focus" href="https://github.com/devmegan/awesome" target="_blank" rel="noreferrer">awesome list</a>. 😎</>
+    header: 'Hi, I\'m Megan',
+    para: 'A Web Developer with hands-on, full-stack experience across public, private and third sector projects.',
+    CTA: <>Want to see what tech I enjoy working with? Check out my <a className="link link-hover font-semibold underline text-secondary hover:text-secondary-focus" href="https://github.com/devmegan/awesome" target="_blank" rel="noreferrer">awesome list</a>.</>
 }
 export { LINKS, HERO_CONTENT, GITHUB_LINK };
